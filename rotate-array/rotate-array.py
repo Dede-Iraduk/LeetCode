291fbd414b858196677dc002b47a4897bf983a6b
Delicia <70854589+Dede-Iraduk@users.noreply.github.com>
2024-06-12 12:42:04 -0700

    Time: 147 ms, Memory: 27.7 MB - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -4,6 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
+        # ensuring k doesn't go out of bound
         k = k % n
+        # nums[:k]: get the last k elements of the array != nums[:k]: get elements from the first one to the k element and concatenating the two slices
         nums[:] = nums[-k:] + nums[:-k]
         
